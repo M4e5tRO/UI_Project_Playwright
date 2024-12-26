9cc9dd1dcc0ac1b959ad6ec19fc7dba66e8f3627
Mikhail Luzan <mikhail.luzan.2021@gmail.com>
2024-12-26 17:17:13 +0300

    add-pytest-mark

--- tests/test_eco_friendly.py
+++ tests/test_eco_friendly.py
@@ -1,4 +1,5 @@
 import allure
+import pytest
 
 from ..test_data import data_list as dl
 
@@ -6,6 +7,7 @@ from ..test_data import data_list as dl
 @allure.feature('Eco Friendly')
 class TestEcoFriendly:
 
+    @pytest.mark.smoke
     @allure.story('Test Sorting By Product Name')
     def test_sort_by_name(self, eco_friendly):
         eco_friendly.open_page()
@@ -15,6 +17,7 @@ class TestEcoFriendly:
         eco_friendly.switch_asc()
         eco_friendly.check_sort_by_name(order='asc')
 
+    @pytest.mark.smoke
     @allure.story('Test Sorting By Price')
     def test_sort_by_price(self, eco_friendly):
         eco_friendly.open_page()
@@ -24,6 +27,7 @@ class TestEcoFriendly:
         eco_friendly.switch_asc()
         eco_friendly.check_sort_by_price(order="asc")
 
+    @pytest.mark.regression
     @allure.story('Test Paging')
     def test_paging(self, eco_friendly):
         eco_friendly.open_page()
@@ -31,6 +35,7 @@ class TestEcoFriendly:
         eco_friendly.check_paging_prev()
         eco_friendly.check_paging_item()
 
+    @pytest.mark.extended
     @allure.story('Test Limiter')
     def test_limiter(self, eco_friendly):
         eco_friendly.open_page()
